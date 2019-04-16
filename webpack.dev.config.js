318a84a2a9ab6176ebc9913d9a6c6d3a3fd0ae94
Victor Zhou <vzhou842@gmail.com>
2019-04-16 15:58:37 -0400

    Integrate babel-loader

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,5 +8,19 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
