71d51adc15c568f26b917417d19c65c9b09ea1a7
Victor Zhou <vzhou842@gmail.com>
2019-04-19 17:34:07 -0400

    Add Bootstrap warning comment

--- src/client/index.js
+++ src/client/index.js
@@ -5,6 +5,9 @@ import { downloadAssets } from './assets';
 import { initState } from './state';
 import { setLeaderboardHidden } from './leaderboard';
 
+// I'm using Bootstrap here for convenience, but I wouldn't recommend actually doing this for a real
+// site. It's heavy and will slow down your site - either only use a subset of Bootstrap, or just
+// write your own CSS.
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.css';
 
