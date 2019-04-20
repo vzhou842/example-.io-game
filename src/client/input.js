50a586760a8ff3e21c4467f91063d1822d362559
Victor Zhou <vzhou842@gmail.com>
2019-04-19 22:00:25 -0400

    Add support for touch events

--- src/client/input.js
+++ src/client/input.js
@@ -1,16 +1,29 @@
 import { updateDirection } from './networking';
 
 function onMouseInput(e) {
-  const dir = Math.atan2(e.clientX - window.innerWidth / 2, window.innerHeight / 2 - e.clientY);
+  handleInput(e.clientX, e.clientY);
+}
+
+function onTouchInput(e) {
+  const touch = e.touches[0];
+  handleInput(touch.clientX, touch.clientY);
+}
+
+function handleInput(x, y) {
+  const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
   updateDirection(dir);
 }
 
 export function startCapturingInput() {
   window.addEventListener('mousemove', onMouseInput);
   window.addEventListener('click', onMouseInput);
+  window.addEventListener('touchstart', onTouchInput);
+  window.addEventListener('touchmove', onTouchInput);
 }
 
 export function stopCapturingInput() {
   window.removeEventListener('mousemove', onMouseInput);
   window.removeEventListener('click', onMouseInput);
+  window.removeEventListener('touchstart', onTouchInput);
+  window.removeEventListener('touchmove', onTouchInput);
 }
