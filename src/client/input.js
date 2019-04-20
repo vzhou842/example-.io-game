c30364ed52ed565306871e33ba4ae2c3b75a9654
Victor Zhou <vzhou842@gmail.com>
2019-04-19 21:41:31 -0400

    Add support for click input events

--- src/client/input.js
+++ src/client/input.js
@@ -1,14 +1,18 @@
 import { updateDirection } from './networking';
 
-function onMouseMove(e) {
+function onMouseInput(e) {
   const dir = Math.atan2(e.clientX - window.innerWidth / 2, window.innerHeight / 2 - e.clientY);
   updateDirection(dir);
 }
 
 export function startCapturingInput() {
-  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mousemove', onMouseInput);
+  window.addEventListener('click', onMouseInput);
+
+
 }
 
 export function stopCapturingInput() {
-  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mousemove', onMouseInput);
+  window.removeEventListener('click', onMouseInput);
 }
