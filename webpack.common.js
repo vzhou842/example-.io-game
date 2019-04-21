fe7f7900888050940537344b8d60715aa7df880c
Victor Zhou <vzhou842@gmail.com>
2019-04-21 15:26:05 -0400

    Remove unused webpack option

--- webpack.common.js
+++ webpack.common.js
@@ -26,9 +26,6 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: true,
-            },
           },
           'css-loader',
         ],
