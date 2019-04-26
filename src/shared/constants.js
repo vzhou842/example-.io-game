6ee83aafb35cee9070cbe2b51b94f83c31ed3331
Victor Zhou <vzhou842@gmail.com>
2019-04-25 21:07:59 -0400

    Reduce MAP_SIZE to 3000

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -11,7 +11,7 @@ module.exports = Object.freeze({
   SCORE_BULLET_HIT: 20,
   SCORE_PER_SECOND: 1,
 
-  MAP_SIZE: 4000,
+  MAP_SIZE: 3000,
   MSG_TYPES: {
     JOIN_GAME: 'join_game',
     GAME_UPDATE: 'update',
