5cd6c6a6a2b99352b4f720cdb682cfaff92656b2
Victor Zhou <vzhou842@gmail.com>
2019-06-09 16:31:55 -0500

    Add Flow typing for client networking.js

--- src/client/networking.js
+++ src/client/networking.js
@@ -1,3 +1,4 @@
+// @flow
 // Learn more about this file at:
 // https://victorzhou.com/blog/build-an-io-game-part-1/#4-client-networking
 import io from 'socket.io-client';
@@ -15,22 +16,22 @@ const connectedPromise = new Promise(resolve => {
   });
 });
 
-export const connect = onGameOver => (
+export const connect = (onGameOver: Function) => (
   connectedPromise.then(() => {
     // Register callbacks
     socket.on(Constants.MSG_TYPES.GAME_UPDATE, processGameUpdate);
     socket.on(Constants.MSG_TYPES.GAME_OVER, onGameOver);
     socket.on('disconnect', () => {
       console.log('Disconnected from server.');
-      document.getElementById('disconnect-modal').classList.remove('hidden');
-      document.getElementById('reconnect-button').onclick = () => {
+      (document.getElementById('disconnect-modal'): any).classList.remove('hidden');
+      (document.getElementById('reconnect-button'): any).onclick = () => {
         window.location.reload();
       };
     });
   })
 );
 
-export const play = username => {
+export const play = (username: string) => {
   socket.emit(Constants.MSG_TYPES.JOIN_GAME, username);
 };
 
