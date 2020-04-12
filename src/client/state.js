72001432a5d2aa3aa8470482755a901740cf9b28
unknown <emersonye@gmail.com>
2020-04-12 13:17:43 -0700

    Only interpolate numeric values.

--- src/client/state.js
+++ src/client/state.js
@@ -81,6 +81,8 @@ function interpolateObject(object1, object2, ratio) {
   Object.keys(object1).forEach(key => {
     if (key === 'direction') {
       interpolated[key] = interpolateDirection(object1[key], object2[key], ratio);
+    } else if (Number.isNaN(object1[key])) {
+      interpolated[key] = ratio > 0.5 ? object2[key] : object1[key];
     } else {
       interpolated[key] = object1[key] + (object2[key] - object1[key]) * ratio;
     }
