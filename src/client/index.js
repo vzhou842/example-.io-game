02056e0b0166640daaccd44326120e92779ca1a2
larrygao2000 <larrygao2000@gmail.com>
2021-02-01 03:41:38 +0000

    Update leaderboard with place Update Play menu to accept enter key

--- src/client/index.js
+++ src/client/index.js
@@ -17,6 +17,16 @@ const playMenu = document.getElementById('play-menu');
 const playButton = document.getElementById('play-button');
 const usernameInput = document.getElementById('username-input');
 
+usernameInput.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    playButton.click();
+  }
+});
+
 Promise.all([
   connect(onGameOver),
   downloadAssets(),
--- src/client/leaderboard.js
+++ src/client/leaderboard.js
@@ -7,12 +7,12 @@ export function updateLeaderboard(data) {
   // This is a bit of a hacky way to do this and can get dangerous if you don't escape usernames
   // properly. You would probably use something like React instead if this were a bigger project.
   for (let i = 0; i < data.length; i++) {
-    rows[i + 1].innerHTML = `<td>${escape(data[i].username.slice(0, 15)) || 'Anonymous'}</td><td>${
+    rows[i + 1].innerHTML = `<td>${i+1}</td><td>${escape(data[i].username.slice(0, 15)) || 'Anonymous'}</td><td>${
       data[i].score
     }</td>`;
   }
-  for (let i = data.length; i < 5; i++) {
-    rows[i + 1].innerHTML = '<td>-</td><td>-</td>';
+  for (let i = data.length; i < 6; i++) {
+    rows[i + 1].innerHTML = '<td>-</td><td>-</td><td>-</td>';
   }
 }
 
