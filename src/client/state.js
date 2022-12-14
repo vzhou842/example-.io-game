632374108133edf52afa491274def13cae73e53e
Divide by Zero <nnnevermind@protonmail.com>
2022-12-13 20:59:40 -0500

    add placeholder mfer

--- src/client/state.js
+++ src/client/state.js
@@ -81,7 +81,7 @@ function interpolateObject(object1, object2, ratio) {
 
   const interpolated = {};
   Object.keys(object1).forEach(key => {
-    if (key === "id") {interpolated[key] = object1[key];}
+    if (key === "tokenId") {interpolated[key] = object1[key];}
     else if (key === 'direction') {
       interpolated[key] = interpolateDirection(object1[key], object2[key], ratio);
     } else {
